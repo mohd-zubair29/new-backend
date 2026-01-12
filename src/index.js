f00af685f64bcb5114dd74fcf17d33e248991636
Mohd Zubair <zubzubair29@gmail.com>
2026-01-12 21:29:13 +0530

    utils structure

--- src/index.js
+++ src/index.js
@@ -6,4 +6,11 @@ import connectDB from "./db/index.js"
 
 dotenv.config({path:'./env'})
 
-connectDB();
+connectDB()
+.then( ()=>{
+    app.listen(`App Is Listening At Port ${process.env.PORT}`)
+})
+.catch((error)=>{
+    console.log("Error In Connecting To Database", error);
+})
+
